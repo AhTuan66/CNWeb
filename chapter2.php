7cf3d8f5fbd7c52ab6a3008291861ddcf7f5654e
AhTuan66 <anhtuan160205@gmail.com>
2025-11-18 21:31:29 +0700

    mPHT Buoi 1 chuong 2

---
+++ chapter2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="vi">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHT Chương 2 - PHP Căn Bản</title>
+    </head>
+    <body>
+        <h1>Kết quả PHP Căn Bản</h1>
+        <?php
+            include "sinhvien.php";
+        ?>
+    </body>
+</html>
